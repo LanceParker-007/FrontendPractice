21f0e864550d0e50f1d04291ebaf732136d6cc9a
lance <lanceparker2001@gmail.com>
2024-05-22 08:51:03 +0530

    studying-call,apply,bind from roadside coder 2

--- PracticeQuestions/roadsideCoder/callApplyBind.js
+++ PracticeQuestions/roadsideCoder/callApplyBind.js
@@ -90,3 +90,61 @@ function sayHello(strVal, strVal2) {
 // };
 
 // console.log(sayHello.myApply2(obj, ["apply1+", "appl2+"]));
+
+// Q1: Tell Output
+// const person = {
+//   name: "Piyush",
+// };
+
+// function sayHi(age) {
+//   return `${this.name} is ${age}`;
+// }
+
+// console.log(sayHi.call(person, 24)); //Piyush is 24
+// console.log(sayHi.bind(person, 24)); // [Function: bound sayHi]
+
+// Q2:
+
+// const age = 10;
+// var person = {
+//   name: "Piyush",
+//   age: 20,
+//   getAge: function () {
+//     return this.age;
+//   },
+// };
+
+// var person2 = { age: 24 };
+// console.log(person.getAge.call(person2)); // 24
+// console.log(person.getAge.apply(person2)); // 24
+// console.log(person.getAge.bind(person2)()); // 24
+
+// Q3:
+// var status = "😎";
+
+// setTimeout(() => {
+//   const status = "😍";
+
+//   const data = {
+//     status: "🥑",
+//     getStatus() {
+//       return this.status;
+//     },
+//   };
+
+//   console.log(data.getStatus());
+//   console.log(data.getStatus.call(this));
+// }, 0);
+
+// Q4:
+const animals = [
+  { species: "Lion", name: "King" },
+  { species: "Whale", name: "Queen" },
+];
+
+function printAnimals(i) {
+  this.print = function () {
+    console.log("#" + i + " " + this.species + ": " + this.name);
+  };
+  this.print();
+}
